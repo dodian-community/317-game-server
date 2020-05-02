d2166ac9e3386a68bf320df0b8702f51773f2fd2
Nozemi <nozemi95@gmail.com>
2020-05-02 16:22:45 +0200

    Quick Fix - Teleports Plugin

--- game-server/src/main/java/net/dodian/extend/plugins/spellbook/Teleports.java
+++ game-server/src/main/java/net/dodian/extend/plugins/spellbook/Teleports.java
@@ -6,9 +6,11 @@ import net.dodian.old.world.entity.impl.player.Player;
 import net.dodian.old.world.model.Position;
 import net.dodian.old.world.model.teleportation.TeleportHandler;
 import net.dodian.old.world.model.teleportation.TeleportType;
+import org.springframework.stereotype.Component;
 
 import static net.dodian.GameConstants.DEFAULT_POSITION;
 
+@Component
 public class Teleports implements ButtonClickEventListener {
 
     @Override
