df91a96ec37bf84f2631da3e2944fdaf3ddd5583
Nozemi <nozemi95@gmail.com>
2020-05-05 22:35:20 +0200

    Packet Handling & Ancient Teleports - Added a packet to handle button clicks - Added a packet for player options - Added a plugin for Ancient spell book - Added button in the PlayerButtonClickEvent - Removed unnecessary method in NpcOptionPacketHandler

--- game-server/src/main/java/net/dodian/packets/handlers/impl/npc/NpcOptionPacketHandler.java
+++ game-server/src/main/java/net/dodian/packets/handlers/impl/npc/NpcOptionPacketHandler.java
@@ -10,34 +10,26 @@ import net.dodian.packets.handlers.PacketListener;
 import net.dodian.packets.impl.npc.*;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
-
 @Component
 public class NpcOptionPacketHandler extends PacketListener {
 
-    public Optional<NPC> getTarget(NpcOptionPacket packet) {
-        if(packet.getNpcIndex() < 0 || packet.getNpcIndex() > World.getNpcs().capacity()) {
-            return Optional.empty();
-        }
-
-        final NPC npc = World.getNpcs().get(packet.getNpcIndex());
-
-        if(npc == null) {
-            return Optional.empty();
-        }
-
-        return Optional.of(npc);
-    }
-
     public <T extends PlayerNpcEvent> void handleClick(T event, NpcOptionPacket packet) {
-        getTarget(packet).ifPresent(target -> {
-            packet.getPlayer().setEntityInteraction(target);
-            packet.getPlayer().setWalkToTask(new WalkToTask(packet.player, target.getPosition(), target.getSize(), () -> {
-                eventsProvider.executeListeners(event.create(packet.getPlayer(), target));
-                packet.getPlayer().setPositionToFace(target.getPosition());
-                target.setPositionToFace(packet.getPlayer().getPosition());
-            }));
-        });
+        if(packet.getNpcIndex() < 0 || packet.getNpcIndex() > World.getNpcs().capacity()) {
+            return;
+        }
+
+        final NPC target = World.getNpcs().get(packet.getNpcIndex());
+
+        if(target == null) {
+            return;
+        }
+
+        packet.getPlayer().setEntityInteraction(target);
+        packet.getPlayer().setWalkToTask(new WalkToTask(packet.player, target.getPosition(), target.getSize(), () -> {
+            eventsProvider.executeListeners(event.create(packet.getPlayer(), target));
+            packet.getPlayer().setPositionToFace(target.getPosition());
+            target.setPositionToFace(packet.getPlayer().getPosition());
+        }));
     }
 
     @PacketHandler
