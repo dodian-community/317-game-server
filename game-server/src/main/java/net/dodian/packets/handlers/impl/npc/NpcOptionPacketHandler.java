415bf48f6d56476e900a752f1013a1d57355ee4e
Nozemi <nozemi95@gmail.com>
2020-05-05 12:35:10 +0200

    Packets & Packet Handlers - One class per opcode (smoother to implement packet handlers) - Did a few packet handlers (the same way that they were done in Elvarg from before)

--- game-server/src/main/java/net/dodian/packets/handlers/impl/npc/NpcOptionPacketHandler.java
+++ game-server/src/main/java/net/dodian/packets/handlers/impl/npc/NpcOptionPacketHandler.java
@@ -1,9 +1,33 @@
 package net.dodian.packets.handlers.impl.npc;
 
+import net.dodian.packets.handlers.PacketHandler;
 import net.dodian.packets.handlers.PacketListener;
+import net.dodian.packets.impl.npc.NpcFirstOptionPacket;
+import net.dodian.packets.impl.npc.NpcFourthOptionPacket;
+import net.dodian.packets.impl.npc.NpcSecondOptionPacket;
+import net.dodian.packets.impl.npc.NpcThirdOptionPacket;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NpcOptionPacketHandler implements PacketListener {
 
+    @PacketHandler
+    public void onFirstClick(NpcFirstOptionPacket packet) {
+
+    }
+
+    @PacketHandler
+    public void onSecondClick(NpcSecondOptionPacket packet) {
+
+    }
+
+    @PacketHandler
+    public void onThirdClick(NpcThirdOptionPacket packet) {
+
+    }
+
+    @PacketHandler
+    public void onFourthClick(NpcFourthOptionPacket packet) {
+
+    }
 }
