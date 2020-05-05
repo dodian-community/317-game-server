f76e7aaa96cdfef15ca699eca657fbe75f5590c9
Nozemi <nozemi95@gmail.com>
2020-05-05 15:02:41 +0200

    NpcOptionsPacket Handling & Added Command Movement Opcode to GameMovementPacket

--- game-server/src/main/java/net/dodian/packets/handlers/impl/npc/NpcOptionPacketHandler.java
+++ game-server/src/main/java/net/dodian/packets/handlers/impl/npc/NpcOptionPacketHandler.java
@@ -1,33 +1,70 @@
 package net.dodian.packets.handlers.impl.npc;
 
+import net.dodian.events.EventsProvider;
+import net.dodian.events.impl.player.interact.npc.*;
+import net.dodian.old.engine.task.impl.WalkToTask;
+import net.dodian.old.world.World;
+import net.dodian.old.world.entity.impl.npc.NPC;
 import net.dodian.packets.handlers.PacketHandler;
 import net.dodian.packets.handlers.PacketListener;
-import net.dodian.packets.impl.npc.NpcFirstOptionPacket;
-import net.dodian.packets.impl.npc.NpcFourthOptionPacket;
-import net.dodian.packets.impl.npc.NpcSecondOptionPacket;
-import net.dodian.packets.impl.npc.NpcThirdOptionPacket;
+import net.dodian.packets.impl.npc.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class NpcOptionPacketHandler implements PacketListener {
 
+    private final EventsProvider eventsProvider;
+
+    @Autowired
+    public NpcOptionPacketHandler(EventsProvider eventsProvider) {
+        this.eventsProvider = eventsProvider;
+    }
+
+    public Optional<NPC> getTarget(NpcOptionPacket packet) {
+        if(packet.getNpcIndex() < 0 || packet.getNpcIndex() > World.getNpcs().capacity()) {
+            return Optional.empty();
+        }
+
+        final NPC npc = World.getNpcs().get(packet.getNpcIndex());
+
+        if(npc == null) {
+            return Optional.empty();
+        }
+
+        return Optional.of(npc);
+    }
+
+    public <T extends PlayerNpcEvent> void handleClick(T event, NpcOptionPacket packet) {
+        getTarget(packet).ifPresent(target -> {
+            packet.getPlayer().setEntityInteraction(target);
+            packet.getPlayer().setWalkToTask(new WalkToTask(packet.player, target.getPosition(), target.getSize(), () -> {
+                eventsProvider.executeListeners(event.create(packet.getPlayer(), target));
+                packet.getPlayer().setPositionToFace(target.getPosition());
+                target.setPositionToFace(packet.getPlayer().getPosition());
+            }));
+        });
+    }
+
     @PacketHandler
     public void onFirstClick(NpcFirstOptionPacket packet) {
-
+        handleClick(new PlayerNpcFirstClickEvent(), packet);
     }
 
     @PacketHandler
     public void onSecondClick(NpcSecondOptionPacket packet) {
-
+        handleClick(new PlayerNpcFirstClickEvent(), packet);
     }
 
     @PacketHandler
     public void onThirdClick(NpcThirdOptionPacket packet) {
-
+        handleClick(new PlayerNpcFirstClickEvent(), packet);
     }
 
     @PacketHandler
     public void onFourthClick(NpcFourthOptionPacket packet) {
-
+        handleClick(new PlayerNpcFirstClickEvent(), packet);
     }
 }
