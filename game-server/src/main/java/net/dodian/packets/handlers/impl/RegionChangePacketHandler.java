c5e8178c24e18e83e11e516fc6bafe3f90e88b9d
Snook <Dsnook100@gmail.com>
2020-05-16 19:51:23 -0400

    Added RegionChangePacketHandler to allow for dropped items to reappear when leaving the region and coming back. Added ChatPacketHandler to handle sending basic chat messages.

---
+++ game-server/src/main/java/net/dodian/packets/handlers/impl/RegionChangePacketHandler.java
@@ -0,0 +1,20 @@
+package net.dodian.packets.handlers.impl;
+
+import net.dodian.old.world.entity.impl.player.Player;
+import net.dodian.old.world.grounditems.GroundItemManager;
+import net.dodian.packets.handlers.PacketHandler;
+import net.dodian.packets.handlers.PacketListener;
+import net.dodian.packets.impl.RegionChangePacket;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RegionChangePacketHandler extends PacketListener {
+    @PacketHandler
+    public void onRegionChange(RegionChangePacket packet) {
+        Player player = packet.getPlayer();
+        if (player.isAllowRegionChangePacket()) {
+            GroundItemManager.onRegionChange(player);
+            player.setAllowRegionChangePacket(false);
+        }
+    }
+}
